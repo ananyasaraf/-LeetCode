6f39fd20a55bab51944f6ba420d31803b36293ee
ananyasaraf <90857982+ananyasaraf@users.noreply.github.com>
2024-07-16 21:34:51 +0530

    Create PeakIndexinaMountainArray.java

---
+++ PeakIndexinaMountainArray.java
@@ -0,0 +1,21 @@
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+  
+    int peakIndexInMountainArray(vector<int> v) {
+	int s = 0, e = v.size() - 1;
+	int mid=(s + e) / 2;
+	while (s < e)
+	{
+		if(v[mid] < v[mid+1])
+     			s=mid+1;
+    		else
+      			e = mid;
+    
+		mid = (s + e) / 2;
+	}
+	return s;
+    }
+};
